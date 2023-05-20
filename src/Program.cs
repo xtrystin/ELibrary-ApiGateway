67e9bb1d5399f9c6f17d4689f3be12f7ac0f1eef
xtrystin <kamsil12>
2023-05-20 13:37:18 +0200

    Adds CI/CD pipeline & changed CORS policy

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,13 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(policy =>
+{
+    policy.AddPolicy("OpenCorsPolicy", opt =>
+        opt.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -24,7 +31,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("OpenCorsPolicy");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 
